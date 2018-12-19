1a1bc36605f717de741cb60a4c53a92107af48b5
Alexey Vorobev <vorobalek@icloud.com>
2018-12-19 17:18:52 +0300

    Рефакторинг. Публикация всё ещё не работает

--- setup.bat
+++ setup.bat
@@ -4,11 +4,8 @@ rd /s /q "..\TL Engine Developer"
 rd /s /q "TL.Engine\Data"
 rd /s /q "TL.Engine\Extensions"
 rd /s /q "TL.Engine\Migrations"
-
 dotnet build -c Release
-
 xcopy ".\TL.Clean" ".\TL.Engine" /I /H /Y /C /R /S /EXCLUDE:.\.list_no_copy_clean
-
 @echo off
 (echo.0)>.last_migration_version
 @echo on
@@ -17,7 +14,8 @@ dotnet ef migrations add InitialCreate
 dotnet ef database update
 cd "..\"
 version_inc.exe .last_migration_version
-
+rd /s /q "TL.Engine\Extensions"
+dotnet build -c Release
 call copy_all.bat
 
 pause
--- upgrade.bat
+++ upgrade.bat
@@ -1,9 +1,7 @@
 xcopy "..\TL Engine Developer\netcoreapp2.2\Data" ".\TL.Engine\Data" /I /H /Y /C /R /S
 rd /s /q "..\TL Engine Developer"
 rd /s /q "TL.Engine\Extensions"
-
 dotnet build -c Release
-
 for /F %%F in (.last_migration_version) do (
 	cd "TL.Engine"
 	dotnet ef migrations add MigrationUpdate%%F
@@ -11,7 +9,8 @@ for /F %%F in (.last_migration_version) do (
 	cd "..\"
 	version_inc.exe .last_migration_version
 )
-
+rd /s /q "TL.Engine\Extensions"
+dotnet build -c Release
 call copy_all.bat
 
 pause
