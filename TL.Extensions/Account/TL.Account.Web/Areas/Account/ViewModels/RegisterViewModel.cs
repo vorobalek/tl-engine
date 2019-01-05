26f5f59f602c7b3f136e779f41111d752ef12712
Alexey Vorobev <vorobalek@icloud.com>
2019-01-05 20:57:38 +0300

    Исправлены скрипты валидации

--- TL.Extensions/Account/TL.Account.Web/Areas/Account/ViewModels/RegisterViewModel.cs
+++ TL.Extensions/Account/TL.Account.Web/Areas/Account/ViewModels/RegisterViewModel.cs
@@ -4,13 +4,16 @@ namespace TL.Account.Web.Areas.Account.ViewModels
 {
     public class RegisterViewModel
     {
+        [Display(Name = "Логин")]
         [Required(ErrorMessage = "Не указан логин")]
         public string Username { get; set; }
 
+        [Display(Name = "Пароль")]
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Пароль ещё раз")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароль введен неверно")]
         public string ConfirmPassword { get; set; }
