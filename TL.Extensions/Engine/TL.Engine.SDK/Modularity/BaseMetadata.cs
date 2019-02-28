828be0cbaeae1c8c6361f1494373276872ad4fe7
Alexey Vorobev <vorobalek@icloud.com>
2019-02-28 14:05:19 +0400

    test/v0.5.0.3

--- TL.Extensions/Engine/TL.Engine.SDK/Modularity/BaseMetadata.cs
+++ TL.Extensions/Engine/TL.Engine.SDK/Modularity/BaseMetadata.cs
@@ -7,7 +7,7 @@ namespace TL.Engine.SDK.Modularity
 {
     public abstract class BaseMetadata
     {
-        private Version MinimalVersion => new Version(0, 4, 0, 0);
+        private Version MinimalVersion => new Version(0, 5, 0, 3);
 
         public abstract string Name { get; }
 
